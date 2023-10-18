473c286895d9681730c85d07868ca30cceb4ca54
Murilo Ferreira dos Santos <murilo@jogoglobal.com>
2023-10-18 12:27:48 -0300

    adjust on style Board

--- src/components/Board/Board.vue
+++ src/components/Board/Board.vue
@@ -3,15 +3,32 @@
     <v-col cols="12" md="6" lg="5">
       <div class="board">
         <v-row v-for="row in board" :key="row.id">
-          <v-col v-for="cell in row.cells" :key="cell.id" cols="12" sm="4" md="2" lg="2">
+          <v-col
+            v-for="cell in row.cells"
+            :key="cell.id"
+            cols="12"
+            sm="4"
+            md="2"
+            lg="2"
+          >
             <v-card
               block
               :disabled="cell.revealed || !gameStarted"
-              :class="{ mine: gameEnded && cell.hasMine }"
+              :class="{
+                mine: gameEnded && cell.hasMine,
+                flipped: cell.revealed,
+              }"
               @click="revealCell(cell)"
             >
-              <v-img v-if="cell.revealed && cell.hasMine" :src="mineImage"></v-img>
-              <v-img v-else-if="cell.revealed" :src="gemImage"></v-img>
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <v-img :src="cardBackImage"></v-img>
+                </div>
+                <div class="flip-card-back">
+                  <v-img v-if="cell.hasMine" :src="mineImage"></v-img>
+                  <v-img v-else :src="gemImage"></v-img>
+                </div>
+              </div>
             </v-card>
           </v-col>
         </v-row>
@@ -29,32 +46,79 @@ export default {
   },
   methods: {
     revealCell(cell) {
-      this.$emit('reveal', cell);
-    }
+      this.$emit("reveal", cell);
+    },
   },
   data() {
     return {
       mineImage: require("@/assets/mine.png"),
       gemImage: require("@/assets/gem.png"),
+      cardBackImage: require("@/assets/cardBack.png"),
     };
   },
 };
 </script>
 
 <style scoped>
-/* Estilos para o Tabuleiro */
- .board{
-    justify-content: center;
- }
-.v-card{
-    width: 100px;
-    height: 100px;
+.board {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* height: 100vh; */
 }
 
-.cell {
-    border: 1px solid black;
-    padding: 15px;
-    text-align: center;
+.v-card {
+  max-width: 100px;
+  max-height: 100px;
+  width: auto;
+  height: auto;
+  min-width: 60px;
+  min-height: 60px;
+  flex-grow: 1;
 }
 
+@media (max-width: 600px) {
+  .v-card {
+    max-width: 60px;
+    max-height: 60px;
+  }
+}
+
+.flip-card {
+  transition: transform 0.5s;
+  transform-style: preserve-3d;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transform-style: preserve-3d;
+  transition: transform 0.5s; /* Movendo a transição para este elemento */
+}
+
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+}
+
+.flip-card-front {
+  z-index: 2;
+  transform: rotateY(0deg);
+}
+
+.flipped .flip-card-front {
+  transform: rotateY(180deg); /* Oculte a frente quando virado */
+}
+
+.flip-card-back {
+  transform: rotateY(180deg);
+}
+
+.flipped .flip-card-back {
+  transform: rotateY(360deg); /* Mostre a parte de trás quando virado */
+}
 </style>
